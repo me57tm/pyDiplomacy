4b854ce38de149ef3883657f9f53af4ffcd02484
me57tm <jonathancaines1@gmail.com>
2025-05-25 22:40:18 +0100

    Basic UI

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def twitch_ui():
+    return render_template("index.html")
+
+
+@socketio.event
+def connect():
+    pass
+    #socketio.emit("update_title", {"image": "connected.svg", "title": "UI Connected!"})
---
+++ static/update_page.js
@@ -0,0 +1,35 @@
+ $(document).ready(function() {
+
+    var socket = io();
+
+    socket.on("update_title", function(msg, cb) {
+        //console.log("Got data: " + msg);
+        $("#title-text").text(msg["title"]);
+        $("#title-image").attr("src","/static/title_images/"+msg["image"]);
+    });
+
+    socket.on("add_press", function(msg, cb) {
+        if (msg["type"] == "banner"){
+            $("#press").append('<div class="banner">'+ msg["text"] +'</div>');
+        }
+        else{
+            $("#press").append('<div class="message"><span class="message-head"><span class="sender"><span>From:</span>'+'<img class="press-flag" src="/static/flags/' + msg["sender"] + '.svg"><span>'+msg["sender"]+'</span>'+'</span> <span class="recipient"> To:')
+            for(let i=0; i < msg["recipients"].length; i++){
+                $("#press .message:last-child .recipient").append('<img class="press-flag" src="/static/flags/' + msg["recipients"][i] + '.svg"/><span>'+msg["recipients"][i]+'</span>')
+            }
+            $("#press .message:last-child").append('<span class="message-body"> ' + msg["body"] + '</span>')
+
+        }
+    });
+
+    socket.on("update_screen", function(msg, cb) {
+        if (msg["screen"] == "off"){
+            $(".screen-image").hide();
+        }
+        else{
+           $("#flag").attr("src","/static/flags/" + msg["country_image"]);
+           $("#model").attr("src","/static/logos/" + msg["model_image"]);
+           $(".screen-image").show();
+        }
+    });
+});
